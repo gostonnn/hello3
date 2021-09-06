327f08a057d5edfeacfc01bac4489c581e43e2a1
Vásárhelyi Ágoston <agoston.vasarhelyi@gmail.com>
2021-09-06 09:18:59 +0200

    Modify github user

--- src/App.java
+++ src/App.java
@@ -1,3 +1,13 @@
+/*
+* File: ButtonPanel.java
+* Author: Nagy János
+* Copyright: 2021, Nagy János
+* Group: Szoft V
+* Date: 2021-09-03
+* Github: https://github.com/gostonnn/
+* Licenc: GNU GPL
+*/
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
